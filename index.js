bcc9bd98b7a0083e6aac31a7c47ca2dc3714b5d9
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-16 22:05:31 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   return customerName.toUpperCase();  
+}
+
+function setBestCustomer(){
+    let bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'new customer' 
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'old customer'
+}
